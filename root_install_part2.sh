7d6361155b4eda72b2078a253a680544427bc97a
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-08-30 12:43:43 -0400

    Create root_install_part2.sh

---
+++ root_install_part2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+dpkg --add-architecture i386
+mkdir -pm755 /etc/apt/keyrings
+wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+aptitude update
+aptitude install --install-recommends winehq-staging -y
+
+aptitude install cabextract -y
